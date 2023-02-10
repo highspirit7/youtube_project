0ae48451cebce1b8740c3191b0e19b5f17a58608
GrownUprince <jake.lee4006@gmail.com>
2023-02-10 15:47:49 +0900

    Homepage : apply infinite scrolling by using useInfitineQuery

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,31 +1,54 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 import Card from "components/Card";
 import "./home.scss";
 
 function Home(props) {
   const YOUTUBE_DATA_API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
-  const fetchMostPopularVideos = async () =>
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${YOUTUBE_DATA_API_KEY}`,
-    ).then((res) => res.json());
-  const {
-    isLoading,
-    isError,
-    data: videos,
-    error,
-  } = useQuery(["mostPopular"], fetchMostPopularVideos, {
-    staleTime: 60 * 3 * 1000,
-  });
 
-  if (isLoading)
+  const fetchMostPopularVideos = async ({ pageParam }) => {
+    if (pageParam) {
+      return await axios.get(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${YOUTUBE_DATA_API_KEY}&pageToken=${pageParam}`,
+      );
+    }
+
+    return await axios.get(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${YOUTUBE_DATA_API_KEY}`,
+    );
+  };
+
+  const { data, status, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ["mostPopular"],
+      queryFn: fetchMostPopularVideos,
+      staleTime: 60 * 3 * 1000,
+      getNextPageParam: (lastPage) => {
+        console.log(lastPage.data);
+        return lastPage.data?.nextPageToken;
+      },
+    });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      console.log("inView", inView);
+
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  if (status === "loading")
     return (
       <main className="main-home">
         <h1>is Loading...</h1>
       </main>
     );
 
-  if (isError)
+  if (status === "error")
     return (
       <main className="main-home">
         <h1>Error: {error.message}</h1>
@@ -33,13 +56,23 @@ function Home(props) {
     );
 
   return (
-    <main className="main-home">
-      {videos.items.map((item) => {
-        const { id, snippet } = item;
+    <>
+      <main className="main-home">
+        {data?.pages.map((page, index) => {
+          const { data } = page;
+          return data.items.map((item) => {
+            const { id, snippet } = item;
 
-        return <Card snippet={snippet} videoId={id} key={id} />;
-      })}
-    </main>
+            return <Card snippet={snippet} videoId={id} key={id} />;
+          });
+        })}
+      </main>
+      {isFetchingNextPage ? (
+        <h1 style={{ textAlign: "center", color: "#fff" }}>Loading...</h1>
+      ) : (
+        <div ref={ref} className="invisible-bottom"></div>
+      )}
+    </>
   );
 }
 

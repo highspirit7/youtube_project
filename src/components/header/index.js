5411ad8c05613cfab8495b3bbcbf029caa66ed90
GrownUprince <jake.lee4006@gmail.com>
2023-02-15 17:34:06 +0900

    make logo as button to rouget to home page

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import youtubeIcon from "assets/icons8-youtube-48.png";
 import { RiSearchLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+
+import youtubeIcon from "assets/icons8-youtube-48.png";
 import "./header.scss";
 
 function Header(props) {
   return (
     <header className="header">
-      <h1 className="header__logo">
-        <img src={youtubeIcon} alt="youtube_logo" />
-        Youtube
-      </h1>
+      <Link to={"/"}>
+        <h1 className="header__logo">
+          <img src={youtubeIcon} alt="youtube_logo" />
+          Youtube
+        </h1>
+      </Link>
       <form className="header__form">
         <input type="text" placeholder="Search..." className="header__input" />
         <button className="header__button">
